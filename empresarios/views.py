c5f68a21375cebcfca3be6f0e76c83cd6f295034
lymalucas <34557178+lymalucas@users.noreply.github.com>
2024-08-09 20:39:31 -0300

    fim aula-02

--- empresarios/views.py
+++ empresarios/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Empresas, Documento
+from .models import Empresas, Documento, Metricas
 from django.contrib import messages
 from django.contrib.messages import constants
 
@@ -121,3 +121,18 @@ def excluir_dc(request, id):
     documento.delete()
     messages.add_message(request, constants.SUCCESS, "Documento excluído com sucesso")
     return redirect(f'/empresarios/empresa/{documento.empresa.id}')
+
+def add_metrica(request, id):
+    empresa = Empresas.objects.get(id=id)
+    titulo = request.POST.get("titulo")
+    valor = request.POST.get("valor")
+
+    metrica = Metricas(
+        empresa=empresa,
+        titulo=titulo,
+        valor=valor,
+    )
+    metrica.save()
+
+    messages.add_message(request, constants.SUCCESS, "Métrica cadastrada com sucesso")
+    return redirect(f'/empresarios/empresa/{empresa.id}')
